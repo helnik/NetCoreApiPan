b2f94784d2a5ed60173daa4274d6cb178a8ebc89
Nick Chelmis <nikos.helmis@gmail.com>
2019-01-27 21:34:52 +0200

    Use statusCodes, do not use app.Run for welcome message

---
+++ NetCoreApiPan/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreApiPan.Controllers
+{
+    [ApiController]
+    [Route("api/hello")]
+    public class HelloController : ControllerBase
+    {
+        //Disable from launchsetting.json default behavior (i.e. api/values) let it use a welcome message
+        //instead of using: app.Run(async (context) => { await context.Response.WriteAsync("Welcome to NetCoreApiPan !!"); });
+        //in configure. app.Run is terminal will swallow status codes
+        [HttpGet("getHello")]
+        [ActionName("getHello")]
+        public string GetHello() => "Welcome to NetCoreApiPan API";
+    }
+}
--- NetCoreApiPan/Startup.cs
+++ NetCoreApiPan/Startup.cs
@@ -31,9 +31,11 @@ namespace NetCoreApiPan
             RegisterDIServices(services);
             services
                 .AddMvc(options => RegisterFilters(options.Filters))
-                .AddJsonOptions(options => options.SerializerSettings.Formatting = Formatting.Indented)//indented json response
+                .AddJsonOptions(options =>
+                    options.SerializerSettings.Formatting = Formatting.Indented) //indented json response
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddApplicationPart(typeof(BakingController).Assembly); //or .AddApplicationPart(Assembly.Load("ApiPan.Controllers"));
+                .AddApplicationPart(typeof(BakingController)
+                    .Assembly); //or .AddApplicationPart(Assembly.Load("ApiPan.Controllers"));
         }
 
         private void RegisterDIServices(IServiceCollection services)
@@ -52,6 +54,10 @@ namespace NetCoreApiPan
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //By default, an app doesn't provide a rich status code page for HTTP status codes, such as 404 Not Found.
+            //UseStatusCodePages should be called before request handling middlewares in the pipeline(for example, Static File Middleware and MVC Middleware).
+            app.UseStatusCodePages("text/plain", "Status code page, status code: {0}");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -67,10 +73,6 @@ namespace NetCoreApiPan
             app.UsePrepareMealMiddleware();
             app.UseHttpsRedirection();
             app.UseMvc();
-
-            //Disable from launchsetting.json default behavior (i.e. api/values) let it use a welcome message
-            app.Run(async (context) => { await context.Response.WriteAsync("Welcome to NetCoreApiPan !!"); });
-
         }
     }
 }
