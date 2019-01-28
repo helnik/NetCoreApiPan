c8ee597bb4fcacbb0441a9a5dc3150b49fc5802d
nikolaos chelmis <chelmis.nikolaos@nbg.gr>
2019-01-28 12:56:19 +0200

    Rename field

--- api/ApiPan.Controllers/BakingController.cs
+++ api/ApiPan.Controllers/BakingController.cs
@@ -9,24 +9,24 @@ namespace ApiPan.Controllers
     [Route("api/baking")]
     public class BakingController : ControllerBase
     {
-        private readonly ICooker cooker;
+        private readonly ICooker _cooker;
         public BakingController(ICooker cookerService)
         {
-            cooker = cookerService;
+            _cooker = cookerService;
         }
 
         [HttpGet("getRecipeByMealName/{mealName}")]
         [ActionName("getRecipeByMealName")]
-        public MealsList GetRecipeByMealName(string mealName) => cooker.GetRecipeByMealName(mealName);
+        public MealsList GetRecipeByMealName(string mealName) => _cooker.GetRecipeByMealName(mealName);
 
 
         [HttpGet("startBaking/{temp}")]
         [ActionName("startBaking")]
-        public string StartBaking(int temp) => cooker.StartCooking(temp);
+        public string StartBaking(int temp) => _cooker.StartCooking(temp);
 
         [HttpGet("getBakingStartTime")]
         [ActionName("getBakingStartTime")]
-        public string GetBakingStartTime() => cooker.GetCookingStartTime().ToString("HH:mm:ss");
+        public string GetBakingStartTime() => _cooker.GetCookingStartTime().ToString("HH:mm:ss");
 
         [HttpGet("getDelivery")]
         [ActionName("getDelivery")]
