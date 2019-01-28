43b7b840277b53e41ae27828a89865dc8262b43b
nikolaos chelmis <chelmis.nikolaos@nbg.gr>
2019-01-28 14:59:46 +0200

    Add a post method

--- api/ApiPan.Controllers/BakingController.cs
+++ api/ApiPan.Controllers/BakingController.cs
@@ -5,11 +5,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiPan.Controllers
 {
+    //Add the[FromBody] attribute to the parameter in your ASP.NET Core controller action
+    //Note, if you're using ASP.NET Core 2.1, you can also use the [ApiController] attribute to
+    //automatically infer the [FromBody] binding source for your complex action method parameters.
+    //See the documentation https://docs.microsoft.com/en-us/aspnet/core/web-api/?view=aspnetcore-2.1#binding-source-parameter-inference for details.
     [ApiController]
     [Route("api/baking")]
     public class BakingController : ControllerBase
     {
         private readonly ICooker _cooker;
+
         public BakingController(ICooker cookerService)
         {
             _cooker = cookerService;
@@ -19,6 +24,9 @@ namespace ApiPan.Controllers
         [ActionName("getRecipeByMealName")]
         public MealsList GetRecipeByMealName(string mealName) => _cooker.GetRecipeByMealName(mealName);
 
+        [HttpPost("getOpinionForMeal")]
+        [ActionName("getOpinionForMeal")]
+        public string GetOpinionForMeal(Meal request) => $"{request.StrMeal} is fantastic!";
 
         [HttpGet("startBaking/{temp}")]
         [ActionName("startBaking")]
